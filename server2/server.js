94e0b82544b48941d14bed3a5a573947e870c99d
Jay Bhanushali <jbhanus1@asu.edu>
2023-11-28 20:41:25 -0700

    server.js modified

--- server2/server.js
+++ server2/server.js
@@ -1,3 +1,56 @@
+// var express = require("express");
+// var app = express();
+// var bodyParser = require('body-parser');
+// var errorHandler = require('errorhandler');
+// var methodOverride = require('method-override');
+// var MS = require("mongoskin");
+// var hostname = process.env.HOSTNAME || 'localhost';
+// var port = 8080;
+
+// var accX, accY, accZ, pos;
+
+// var db = MS.db("mongodb://127.0.0.1:27017/sensorData");
+
+// app.get("/", function (req, res) {
+//     res.redirect("index.html")
+// });
+
+// app.get("/sendData", function (req, res) {
+//     accX = req.query.x
+//     accY = req.query.y
+//     accZ = req.query.z
+//     pos = req.query.pos
+//     req.query.time = new Date().getTime();
+//     console.log(req.query);
+    
+
+//    db.collection("data").insertOne(req.query, function(result, err){
+//        console.log(result, err);
+//     res.send("1");
+//    });
+// });
+
+
+// app.get("/getData", function (req, res) {
+//     var ret = {
+//       x: accX,
+//       y: accY,
+//       z: accZ,
+//       pos: pos // Include the posture information
+//     };
+  
+//     res.json(ret); // Send the response as JSON
+//   });
+  
+
+// app.use(methodOverride());
+// app.use(bodyParser());
+// app.use(express.static(__dirname + '/public'));
+// app.use(errorHandler());
+
+// console.log("Simple static server listening at http://" + hostname + ":" + port);
+// app.listen(port);
+
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
@@ -7,7 +60,8 @@ var MS = require("mongoskin");
 var hostname = process.env.HOSTNAME || 'localhost';
 var port = 8080;
 
-var accX, accY, accZ, pos;
+// Use an array to store data for each request
+var sensorData = [];
 
 var db = MS.db("mongodb://127.0.0.1:27017/sensorData");
 
@@ -16,32 +70,31 @@ app.get("/", function (req, res) {
 });
 
 app.get("/sendData", function (req, res) {
-    accX = req.query.x
-    accY = req.query.y
-    accZ = req.query.z
-    pos = req.query.pos
-    req.query.time = new Date().getTime();
-    console.log(req.query);
-    
+    var data = {
+        x: req.query.x,
+        y: req.query.y,
+        z: req.query.z,
+        pos: req.query.pos,
+        time: new Date().getTime()
+    };
 
-   db.collection("data").insertOne(req.query, function(result, err){
-       console.log(result, err);
-    res.send("1");
-   });
+    console.log(data);
+    
+    // Store data in the array
+    sensorData.push(data);
+
+    db.collection("data").insertOne(data, function(result, err){
+        console.log(result, err);
+        res.send("1");
+    });
 });
 
-
 app.get("/getData", function (req, res) {
-    var ret = {
-      x: accX,
-      y: accY,
-      z: accZ,
-      pos: pos // Include the posture information
-    };
-  
-    res.json(ret); // Send the response as JSON
-  });
-  
+    // Retrieve the latest data from the array
+    var latestData = sensorData[sensorData.length - 1];
+
+    res.json(latestData); // Send the response as JSON
+});
 
 app.use(methodOverride());
 app.use(bodyParser());
@@ -50,3 +103,4 @@ app.use(errorHandler());
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
 app.listen(port);
+
