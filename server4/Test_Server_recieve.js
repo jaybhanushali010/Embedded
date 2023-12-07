e9c8e1210ebe5492a0ebf39298f19e579a12c311
Jay Bhanushali <jbhanus1@asu.edu>
2023-12-07 13:21:42 -0700

    server4 file edited

--- server4/Test_Server_recieve.js
+++ server4/Test_Server_recieve.js
@@ -1,44 +1,44 @@
-// server.js
+// // server.js
 
-var express = require("express");
-var app = express();
-var bodyParser = require('body-parser');
-var errorHandler = require('errorhandler');
-var methodOverride = require('method-override');
-var hostname = process.env.HOSTNAME || 'localhost';
-var port = 8080;
+// var express = require("express");
+// var app = express();
+// var bodyParser = require('body-parser');
+// var errorHandler = require('errorhandler');
+// var methodOverride = require('method-override');
+// var hostname = process.env.HOSTNAME || 'localhost';
+// var port = 8080;
 
-// const express = require('express');
-// const bodyParser = require('body-parser');
+// // const express = require('express');
+// // const bodyParser = require('body-parser');
 
 
-const app = express();
-// const port = 3000; // Change this to your desired port
+// const app = express();
+// // const port = 3000; // Change this to your desired port
 
-// Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+// // Parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }));
 
-// Parse application/json
-app.use(bodyParser.json());
+// // Parse application/json
+// app.use(bodyParser.json());
 
-// Serve static files (HTML, CSS, JS)
-app.use(express.static(path.join(__dirname, 'public')));
+// // Serve static files (HTML, CSS, JS)
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// Route to handle the POST request
-app.post('/update_space_counter', (req, res) => {
-    const spaceCounter = req.body.space_counter;
+// // Route to handle the POST request
+// app.post('/update_space_counter', (req, res) => {
+//     const spaceCounter = req.body.space_counter;
 
-    // Handle the received spaceCounter data as needed
-    console.log(`Available Parking space : ${spaceCounter}`);
+//     // Handle the received spaceCounter data as needed
+//     console.log(`Available Parking space : ${spaceCounter}`);
 
-    // Send a response back to the Python script
-    res.status(200).send('Space counter received successfully.');
-});
+//     // Send a response back to the Python script
+//     res.status(200).send('Space counter received successfully.');
+// });
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// // Start the server
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
 
 
 // // --------------------------------------------------------------------------
@@ -79,41 +79,41 @@ app.listen(port, () => {
 
 ///--------------------------------------------------------------------
 
-// var express = require("express");
-// var app = express();
-// var bodyParser = require('body-parser');
-// var errorHandler = require('errorhandler');
-// var methodOverride = require('method-override');
-// var hostname = process.env.HOSTNAME || 'localhost';
-// var port = 8080;
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 8080;
 
-// var parkval;
+var parkval;
 
-// app.get("/", function (req, res) {
-//     res.redirect("index.html");
-// });
+app.get("/", function (req, res) {
+    res.redirect("index.html");
+});
 
-// app.post("/update_space_counter", function (req, res) {
-//     parkval = req.body.space_counter;
-//     // Handle the received spaceCounter data as needed
-//     console.log(`Available parking space: ${parkval}`);
+app.post("/update_space_counter", function (req, res) {
+    parkval = req.body.space_counter;
+    // Handle the received spaceCounter data as needed
+    console.log(`Available parking space: ${parkval}`);
     
-//     // Send a response back to the Python script
-//     res.send("Space counter received successfully.");
-// });
+    // Send a response back to the Python script
+    res.send("Space counter received successfully.");
+});
 
-// app.get("/get_space_counter", function (req, res) {
-//     var ret = {
-//         space_counter: parkval
-//     };
-//     res.send(JSON.stringify(ret));
-// });
+app.get("/get_space_counter", function (req, res) {
+    var ret = {
+        space_counter: parkval
+    };
+    res.send(JSON.stringify(ret));
+});
 
-// app.use(methodOverride());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(express.static(__dirname + '/public'));
-// app.use(errorHandler());
+app.use(methodOverride());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
 
-// console.log("Simple static server listening at http://" + hostname + ":" + port);
-// app.listen(port);
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port);
